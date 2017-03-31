93459ae962e87a21df89d55d894c1ff97042cc9e
Adam Johnston <amj69@case.edu>
2017-03-30 20:09:44 -0500

    moved to 2x2 format, display exactly 5 rows

--- server.py
+++ server.py
@@ -7,12 +7,23 @@ from data_churner import *
 app = Flask(__name__)
 
 
+def add_rankings(l, nmax=5):
+    l[0] = (1, *l[0])
+    for i in range(1, len(l)):
+        l[i] = (l[i-1][0] + (0 if l[i][-1] == l[i-1][-1] else 1), *l[i])
+    l = (l + [('-', '-', '-')] * nmax)[:nmax]
+    return l
+
+
 @app.route('/')
 @lru_cache(maxsize=None)
 def index():
     data = parse_data()
     results = compute_all_ride_results(*data)
     overall = compute_overall_totals(results)
+
+    for category in overall:
+        overall[category] = add_rankings(overall[category])
 
     return render_template('index.html', overall=overall)
 
